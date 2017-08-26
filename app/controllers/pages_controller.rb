a3336f73ba2aafd933a4fe89d4b662b1bc2a1da1
John Hayes <john@ontheinternet.org.uk>
2017-08-26 18:13:41 +0200

    init commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+class PagesController < ApplicationController
+
+def home
+
+if params[:first_name]
+  @message = "Hello #{params[:first_name].capitalize}"
+else
+  @message = "Hello"
+end
+end
+
+end
